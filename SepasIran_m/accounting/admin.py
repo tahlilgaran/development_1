238708007e879d4afae2bb6cc5e477af466f1165
fartahouni <farzanehtahooni@FT.local>
2015-07-28 00:08:21 +0430

    accounting models created and admin fixed.

--- SepasIran_m/accounting/admin.py
+++ SepasIran_m/accounting/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from accounting.models import *
+
+admin.site.register(Trans_Kind1)
+admin.site.register(Trans_Kind2)
+admin.site.register(Trans_Kind3)
 
 # Register your models here.
